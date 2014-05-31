0667354bb15e99e7c57b56413c9844ead045b6cb
salazar <luizhsalazar@gmail.com>
2014-05-31 20:34:25 -0300

    criação projeto grafos

---
+++ src/grafos/Grafo.py
@@ -0,0 +1,18 @@
+'''
+Created on 25/05/2014
+
+@author: salazar
+'''
+
+class Grafo(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+        
---
+++ src/grafos/Vertice.py
@@ -0,0 +1,17 @@
+'''
+Created on 25/05/2014
+
+@author: salazar
+'''
+
+class Vertice(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
